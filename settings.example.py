54d24b53f330421ebe8b279a9104b1d57baba800
Ryan Casey <ryepdx@gmail.com>
2012-11-16 16:52:02 -0800

    Stubbed out FitBit OAuth. Good enough for the weekend.

---
+++ settings.example.py
@@ -0,0 +1,12 @@
+FITBIT_KEY = ''
+FITBIT_SECRET = ''
+TWITTER_KEY = ''
+TWITTER_SECRET = ''
+APP_SECRET_KEY = ''
+DATABASE = ''
+DEBUG = True
+
+# This should not be a thing, but it is for now.
+# Will find a better way to stub out FitBit OAuth later.
+FITBIT_ACCESS_TOKEN = ''
+TWITTER_USERNAME = ''
