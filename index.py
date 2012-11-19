3735a85e27b008222800eb11cfab470ef2273722
George Matter VI <George.Matter.VI@gmail.com>
2012-11-18 16:17:38 -0800

    calculate avg. tweets each hour in an average day

--- index.py
+++ index.py
@@ -1,20 +1,20 @@
 import sqlite3 as db
 from flask import Flask, session, url_for, redirect, render_template, request
-from settings import DEBUG, APP_SECRET_KEY, DATABASE, \
-                     FITBIT_ACCESS_TOKEN, TWITTER_USERNAME, FOURSQUARE_CLIENT_SECRET, \
-		     FOURSQUARE_CALLBACK, FOURSQUARE_CLIENT_ID
+from settings import DEBUG, APP_SECRET_KEY, DATABASE, FITBIT_ACCESS_TOKEN, \
+                     TWITTER_USERNAME, FOURSQUARE_CLIENT_SECRET, \
+                     FOURSQUARE_CALLBACK, FOURSQUARE_CLIENT_ID
 from settings_local import PORT, DEBUG
 
 TWITTER_REQUEST = "https://api.twitter.com/1.1/statuses/user_timeline.json"
 from twitter_auth import twitter_auth, twitter
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests, re
 
 app = Flask(__name__)
 app.register_blueprint(twitter_auth)
 app.secret_key = APP_SECRET_KEY
 
-## NOTA BENE:
+## NOTE:
 ## serving print.html is used to expose code like a print statement
 ##  if availuable, session['test_value'] will be printed
 ##  if availuable, each item in session['test_list'] will be printed
@@ -22,14 +22,20 @@ app.secret_key = APP_SECRET_KEY
 ##      will be printed in place of item
 ##  if availuable, each key/value pair in session['test_dict'] will be printed
 
-def convert_twitter_time(time_string):
+def twitter_time_str_to_datetime(time_string):
     a = re.search("\+[0-9]{4} ", time_string)
     time_string = time_string[:a.start()]+time_string[a.end():].strip()
-    time = datetime.strptime(time_string, "%a %b %d %H:%M:%S %Y")
-    return time.strftime("%Y-%m-%d %H:%M:%S")
+    return datetime.strptime(time_string, "%a %b %d %H:%M:%S %Y")
+
+def format_datetime(dt):
+    return dt.strftime("%Y-%m-%d %H:%M:%S")
 
 def get_tweet_data(user, count):
+    DATA_TO_KEEP = {'id' : 'id',
+                    'timestamp' : 'created_at',
+                    'text' : 'text'}
     tweets = list()
+    userdata = None
     while (count > 0):
         #keep track of how many tweets to get
         get = 30
@@ -43,15 +49,35 @@ def get_tweet_data(user, count):
         resp = twitter.get(TWITTER_REQUEST,
                            data = data,
                            token = session.get('twitter_token'))
-        
+        if not userdata:
+            userdata = resp.data[0]['user']
         for tweet_data in resp.data:
             tweet_slim = dict()
-            tweet_slim['id'] = tweet_data['id']
-            tweet_slim['timestamp'] = convert_twitter_time(tweet_data['created_at'])
-            tweet_slim['text'] = tweet_data['text']
+            for key in DATA_TO_KEEP:
+                tweet_slim[key] = tweet_data[DATA_TO_KEEP[key]]
             tweets.append(tweet_slim)
-    return tweets
-    
+    return userdata, tweets
+
+def calculate_per_day(tweets, offset):
+    latest = twitter_time_str_to_datetime(tweets[0]['timestamp'])
+    earliest = twitter_time_str_to_datetime(tweets[-1]['timestamp'])
+    num_days = (latest-earliest).days
+    if num_days == 0: num_days = 1
+    day = dict()
+    for i in range(24):
+        day[i] = 0
+    for tweet in tweets:
+        dt = twitter_time_str_to_datetime(tweet['timestamp'])
+        dt += timedelta(seconds = offset)
+        day[dt.hour] += 1
+    return [1.0*day[hour]/num_days for hour in day.keys()]
+
+def clean_test_values():
+    if 'test_value' in session: del session['test_value']
+    if 'test_dict' in session: del session['test_dict']
+    if 'test_list' in session: del session['test_list']
+    if 'test_list_value_key' in session: del session['test_list_value_key']
+ 
 @app.route('/')
 def index():
     return redirect('/static/splash/index.html')
@@ -65,11 +91,17 @@ def twitter_demo():
             user = request.args['user']
         #user = "LucianNovo"
         session['user'] = user
-        session['tweets'] = get_tweet_data(user, 200)
-        #session['test_dict'] = request.args
-        #session['test_list'] = session['tweets']
-        #session['test_list_value_key'] = "timestamp"
-        #return render_template('print.html')
+        userdata, tweets = get_tweet_data(user, 300)
+        utc_offset = userdata['utc_offset']
+        average_day = calculate_per_day(tweets, utc_offset)
+        session['tweets'] = tweets
+        session['average_per_day'] = average_day
+        #clean_test_values()
+        #session['test_value'] = 
+        #session['test_dict'] = average_day
+        session['test_list'] = average_day
+        #session['test_list_value_key'] = 
+        return render_template('print.html')
         
     return render_template('twitter.html')
 
