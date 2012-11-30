4edd38f036b2c3ee6146464c51cd606eb960c9e6
Ryan Casey <ryepdx@gmail.com>
2012-11-29 22:06:22 -0800

    Added the Twitter login button to source control.

--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ app = Blueprint('home', __name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('%s/index.html' % app.name)
