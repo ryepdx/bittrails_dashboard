e5c698527b81f38ad109dad879814eef1c399432
Ryan Casey <ryepdx@gmail.com>
2012-12-15 12:42:10 -0800

    Changed the DEBUG setting default value.

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@ APP_SECRET_KEY = 'iABSjQSiQSiYWq8mirZ7c8XxGzl6UBSsm9MFgMR7KUSL9b2Ft3aBsfUkiDUTVp
 BITTRAILS_KEY = u'blYyaguk9ihgMj99kuI2TTmYas5MFt'
 BITTRAILS_SECRET = u'YG02Y8XlPVrv5ccZg17A9LUIzQtsom'
 
-DATABASE = 'bittrails.db'
+DATABASE = 'bittrails'
 PORT = 5001
-DEBUG = True
+DEBUG = False
 
 # Override any of the above settings if they are specified in settings_local.py
 from settings_local import *
